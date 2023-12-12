04e90a63f3a86a4bd3cf80efdd330c01de97f7f7
Ethan Cuenca <cucu7180@colorado.edu>
2023-12-12 12:14:54 -0700

    updated JS project

--- jsProject/project3.js
+++ jsProject/project3.js
@@ -1,97 +1,115 @@
 var phoneNumber = "";
 var availableDigits = [];
 
+// function that will generate random digits 6 so that we have options
 function getRandomDigits() 
 {
     availableDigits = [];
+    var characters = "0123456789!?*";
 
-    while (availableDigits.length < 3) 
+    while (availableDigits.length < 6) 
     {
-      var randomDigit = Math.floor(Math.random() * 10);
+      var randomChar = characters.charAt(Math.floor(Math.random() * characters.length));
 
-      if (!availableDigits.includes(randomDigit)) 
+      if (!availableDigits.includes(randomChar)) 
       {
-        availableDigits.push(randomDigit);
+        availableDigits.push(randomChar);
       }
     }
+
     updateInputDisplay();
 }
 
+// this creates the digit buttons
 function createButton(digit) 
 {
-  var button = document.createElement('div');
-  button.className = 'digitButton';
-  button.textContent = digit;
+    var button = document.createElement('div');
+    button.className = 'digitButton';
+    button.textContent = digit;
+    button.addEventListener('click', function() 
+    {
+      handleButtonClick(button);
+    });
 
-  button.addEventListener('click', function() {
-    handleButtonClick(digit, button);
-  });
-
-  return button;
+    return button;
 }
 
-function handleButtonClick(digit, button) 
+function handleButtonClick(button) 
 {
-  phoneNumber += digit;
-  updateEnteredDigits();
-  button.classList.add('clicked');
-  rerollDigits();
+    if (phoneNumber.length < 10) 
+    {
+      phoneNumber += button.textContent;
+      updateEnteredDigits();
+      getRandomDigits(); // Randomize digits after clicking
+    }
 }
 
+// an update function
 function updateEnteredDigits() 
 {
-  var enteredDigits = document.getElementById('enteredDigits');
-  enteredDigits.textContent = "Entered Digits: " + phoneNumber;
+    var enteredDigits = document.getElementById('enteredDigits');
+    enteredDigits.textContent = "Entered Digits: " + phoneNumber;
 }
 
+// updates the display
 function updateInputDisplay() 
 {
-  var inputContainer = document.getElementById('inputContainer');
-  inputContainer.innerHTML = '';
+    var inputContainer = document.getElementById('inputContainer');
+    inputContainer.innerHTML = '';
 
-  for (var i = 0; i < availableDigits.length; i++) 
-  {
-    var digitButton = createButton(availableDigits[i]);
-    inputContainer.appendChild(digitButton);
-  }
+    for (var i = 0; i < availableDigits.length; i++) 
+    {
+      var digitButton = createButton(availableDigits[i]);
+      inputContainer.appendChild(digitButton);
+    }
 }
 
+// reroll digits and for added difficulty it will also remove the last inputed digit
 function rerollDigits() 
 {
-  getRandomDigits();
-  console.log("Phone Number to Check:", phoneNumber);
+    if (phoneNumber.length > 0) 
+    {
+      phoneNumber = phoneNumber.slice(0, -1); // Remove the last digit
+      updateEnteredDigits();
+    }
+
+    getRandomDigits();
+
+    // originally to check but now not needed
+    // console.log("Phone Number to Check:", phoneNumber);
 }
 
+// simple clear button
 function clearDigits() 
 {
-  phoneNumber = "";
-  updateEnteredDigits();
-  resetClickedState();
+    phoneNumber = "";
+    updateEnteredDigits();
 }
 
-function resetClickedState() 
-{
-  var digitButtons = document.querySelectorAll('.digitButton');
-  digitButtons.forEach(function(button) {
-    button.classList.remove('clicked');
-  });
-}
-
+// simple submit to check your submission
 function submitPhoneNumber() 
 {
-  if (phoneNumber.length === 10) 
-  {
-    alert('Phone number entered successfully: ' + phoneNumber);
-    // You can add further validation logic if needed
-  } 
-  
-  else 
-  {
-    alert('Oops! Try again with a valid 10-digit phone number.');
-  }
-  
-  rerollDigits();
-  resetClickedState();
+    // check to see it is a long enough phone number and that it contains no special characters
+    if (phoneNumber.length === 10) 
+    {
+        if (/^[0-9]+$/.test(phoneNumber)) 
+        {
+            alert('Congratulations! You were able to enter your phone number: ' + phoneNumber);
+            // You can add further validation logic if needed
+        } 
+
+        else 
+        {
+            alert('Oops! Phone number cannot contain special characters.');
+        }
+
+    } 
+
+    // if it is not 10 digits long then this will show
+    else 
+    {
+        alert('Oops! Try again with a valid 10-digit phone number.');
+    }
 }
 
 // Initial setup
